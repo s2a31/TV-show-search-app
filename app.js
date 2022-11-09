bef7098dc2679b4466cb800930b6d9b608670e32
s2a31 <baloghszabolcs43@gmail.com>
2022-11-09 17:55:21 +0200

    Update app.js
    Displaying the TV show image from TV Maze API

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
 const form = document.querySelector('#searchForm');
-form.addEventListener('submit', function (e) {
+form.addEventListener('submit', async function (e) {
   e.preventDefault();
+  const searchTerm = form.elements.query.value;
+  const res = await axios.get(`http://api.tvmaze.com/search/shows?q=${searchTerm}`);
+  const img = document.createElement('img');
+  img.src = res.data[0].show.image.medium;
+  document.body.append(img);
 });
