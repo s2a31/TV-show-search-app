631d1aae6da148b85c5ccf5f1e59c8774a5dd174
s2a31 <baloghszabolcs43@gmail.com>
2022-11-10 17:19:19 +0200

    add style.css
    - added delete functionality when make a new search
    - added some style

--- app.js
+++ app.js
@@ -1,18 +1,38 @@
 const form = document.querySelector('#searchForm');
+const container = document.querySelector('.container');
+
 form.addEventListener('submit', async function (e) {
   e.preventDefault();
+  deleteShows();
   const searchTerm = form.elements.query.value;
-  const res = await axios.get(`http://api.tvmaze.com/search/shows?q=${searchTerm}`);
-  makeImages(res.data);
+  const config = { params: { q: searchTerm } };
+  const res = await axios.get(`http://api.tvmaze.com/search/shows`, config);
+  makeShows(res.data);
   form.elements.query.value = '';
 });
 
-const makeImages = (shows) => {
+const makeShows = (shows) => {
   for (let result of shows) {
-    if (result.show.image) {
+    if (result.show.image && result.show.name) {
+      const show = document.createElement('div');
+      show.classList.add('show');
+      const title = document.createElement('span');
+      title.innerText = result.show.name;
       const img = document.createElement('img');
       img.src = result.show.image.medium;
-      document.body.append(img);
+
+      show.append(img);
+      show.append(title);
+      container.append(show);
+    }
+  }
+};
+
+const deleteShows = () => {
+  if (container.children.length > 0) {
+    const shows = document.querySelectorAll('.show');
+    for (let show of shows) {
+      show.remove();
     }
   }
 };
